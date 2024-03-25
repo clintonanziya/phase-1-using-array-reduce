9f7821be459638b30b68377d4edd51fd26e1b72d
clintonanziya <clintonanziya3@gmail.com>
2024-03-25 11:10:09 +0300

    completed-phase-1-using-array-reduce

--- index.js
+++ index.js
@@ -1,3 +1,10 @@
-const batteryBatches = [4, 5, 3, 4, 4, 6, 5];
+// Given array
+const batteryBatches = [4, 5, 3, 7, 2, 8];
 
-// Code your solution here
+// Using reduce to aggregate the total number of batteries
+const totalBatteries = batteryBatches.reduce(function(total, batch) {
+  return total + batch;
+}, 0);
+
+// Output the total number of batteries
+console.log(totalBatteries); // Output will be the sum of all elements in batteryBatches
